618f345fdda178e37a3b2d89530a2254f45ab260
amandam2017 <amandamaarman19911@gmail.com>
2022-04-05 15:33:08 +0200

    added the logic on price route to get filter with maxPrice

--- index.js
+++ index.js
@@ -35,35 +35,20 @@ app.get('/api/garments', function (req, res) {
 
 })
 // added price route
-app.get('/api/garments/price/:price', function (req, res){
-    res.json(garments.price)
-})
+app.get('/api/garments/price/:price', function(req, res){
+	const maxPrice = Number(req.params.price);
+	const filteredGarments = garments.filter( garment => {
+		// filter only if the maxPrice is bigger than maxPrice
+		if (maxPrice > 0) {
+			return garment.price <= maxPrice;
+		}
+		return true;
+	});
 
-
-// app.get('/api/garments', function(req, res){
-
-// 	const gender = req.query.gender;
-// 	const season = req.query.season;
-
-// 	const filteredGarments = garments.filter(garment => {
-// 		// if both gender & season was supplied
-// 		if (gender != 'All' && season != 'All') {
-// 			return garment.gender === gender 
-// 				&& garment.season === season;
-// 		} else if(gender != 'All') { // if gender was supplied
-// 			return garment.gender === gender
-// 		} else if(season != 'All') { // if season was supplied
-// 			return garment.season === season
-// 		}
-// 		return true;
-// 	});
-
-// 	// note that this route just send JSON data to the browser
-// 	// there is no template
-// 	res.json({ 
-// 		garments : filteredGarments
-// 	});
-// });
+	res.json({ 
+		garments : filteredGarments
+	});
+});
 
 
 // app.get('/api/garments/price/:${price}', function(req, res){
