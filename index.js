598c41bed6e3e1fb163ba3a6d46bc4587d5dc3ec
amandam2017 <amandamaarman19911@gmail.com>
2022-04-05 15:25:30 +0200

    added the filter logic on index.js from line 14 to 34

--- index.js
+++ index.js
@@ -11,17 +11,64 @@ app.use(express.static('public'));
 // API routes to be added here
 app.get('/api/garments', function (req, res) {
 
-    res.json({ garments });
+    const gender = req.query.gender;
+	const season = req.query.season;
+
+	const filteredGarments = garments.filter(garment => {
+		// if both gender & season was supplied
+		if (gender != 'All' && season != 'All') {
+			return garment.gender === gender 
+				&& garment.season === season;
+		} else if(gender != 'All') { // if gender was supplied
+			return garment.gender === gender
+		} else if(season != 'All') { // if season was supplied
+			return garment.season === season
+		}
+		return true;
+	});
+
+	// note that this route just send JSON data to the browser
+	// there is no template
+	res.json({ 
+		garments : filteredGarments
+	});
 
 })
-
+// added price route
 app.get('/api/garments/price/:price', function (req, res){
     res.json(garments.price)
 })
 
-app.get('/api/garments/price/:${price}', function(req, res){
-    res.json({price})
-})
+
+// app.get('/api/garments', function(req, res){
+
+// 	const gender = req.query.gender;
+// 	const season = req.query.season;
+
+// 	const filteredGarments = garments.filter(garment => {
+// 		// if both gender & season was supplied
+// 		if (gender != 'All' && season != 'All') {
+// 			return garment.gender === gender 
+// 				&& garment.season === season;
+// 		} else if(gender != 'All') { // if gender was supplied
+// 			return garment.gender === gender
+// 		} else if(season != 'All') { // if season was supplied
+// 			return garment.season === season
+// 		}
+// 		return true;
+// 	});
+
+// 	// note that this route just send JSON data to the browser
+// 	// there is no template
+// 	res.json({ 
+// 		garments : filteredGarments
+// 	});
+// });
+
+
+// app.get('/api/garments/price/:${price}', function(req, res){
+//     res.json({price})
+// })
 
 
 const PORT = process.env.PORT || 4017;
